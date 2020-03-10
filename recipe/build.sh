bf1ade9f85e1b6be40bf1ce7c13e2f6ebc2d4965
Jason Furmanek <furmanek@us.ibm.com>
2020-03-10 19:30:04 +0000

    fix migrator(cos7 updates)

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 export CFLAGS="-I${PREFIX}/include"
-export LDFLAGS="-L${PREFIX}/lib"
+#export LDFLAGS="-L${PREFIX}/lib"
+export LDFLAGS="${LDFLAGS} -Wl,-rpath-link,${PREFIX}/lib"
 ./configure  --prefix=$PREFIX ##--disable-static
 make
 make install
