75b4c5e49f29e1e9f586c2d9cc36d1ab8d211a83
Jason Furmanek <furmanek@us.ibm.com>
2020-03-12 13:50:28 +0000

    LDFLAGS switch

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 export CFLAGS="-I${PREFIX}/include"
-#export LDFLAGS="-L${PREFIX}/lib"
-export LDFLAGS="${LDFLAGS} -Wl,-rpath-link,${PREFIX}/lib"
+if [[ ${HOST} =~ .*powerpc64le.* ]] || [[ ${HOST} =~ .*aarch64.* ]]; then
+  export LDFLAGS="${LDFLAGS} -Wl,-rpath-link,${PREFIX}/lib"
+  echo "cos7 architecture"
+  echo "LDFLAGS=${LDFLAGS}"
+else
+  export LDFLAGS="-L${PREFIX}/lib"
+  echo "cos6 architecture"
+  echo "LDFLAGS=${LDFLAGS}"
+fi
 ./configure  --prefix=$PREFIX ##--disable-static
 make
 make install
