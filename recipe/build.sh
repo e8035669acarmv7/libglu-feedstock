080e07575846ba211aee8cd5b413f11943811198
Jason Furmanek <furmanek@us.ibm.com>
2020-03-24 05:36:10 +0000

    add xcb

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
 export CFLAGS="-I${PREFIX}/include"
-if [[ ${HOST} =~ .*powerpc64le.* ]] || [[ ${HOST} =~ .*aarch64.* ]]; then
-  export LDFLAGS="${LDFLAGS} -Wl,-rpath-link,${PREFIX}/lib"
-  echo "cos7 architecture"
-  echo "LDFLAGS=${LDFLAGS}"
-else
-  export LDFLAGS="-L${PREFIX}/lib"
-  echo "cos6 architecture"
-  echo "LDFLAGS=${LDFLAGS}"
-fi
+export LDFLAGS="-L${PREFIX}/lib"
 ./configure  --prefix=$PREFIX ##--disable-static
 make
 make install
